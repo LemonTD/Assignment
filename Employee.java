94845f9b9ef6ccc1305ba084f24cdb146737e8e7
Lemon Toby <50625370+LemonTD@users.noreply.github.com>
2020-01-13 15:31:34 +0100

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -1,16 +1,7 @@
 public class Employee{
-    private String firstName;
-    private String lastName;
-    private double monthlySalary;
-
-    
-    public void invoice(){
-        System.out.println("test");
-    }
     
     public static void main(String[] args){
-    Employee b = new Employee();
         
-        b.invoice().
+        System.out.print("Trial with jenkins");
     }
 }
